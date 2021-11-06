d67c8a5f17be10e7e278df2f39a45dcc3d6b82c6
Faiza Siddiqui <faizasiddiqui@uregina.ca>
2021-11-06 22:54:43 +0300

    authorship comments & description of program

--- ShakerSort.cpp
+++ ShakerSort.cpp
@@ -1,3 +1,18 @@
+/*****************************************************************************************************************************
+	Module Name: Shaker Sort Algorithm
+	Author: Faiza Fatma Siddiqui
+	Date Created/Modified:11.11.2021
+	Student ID: 200473896
+	Purpose: CS 700 - Software Development Fundamentals - Assignment 4 
+	Description: 
+		- This program prompts the user to determine the size of array. 
+		- It restricts the user from entering any invalid entries for size of array and allows to enter only positive whole numbers.
+		- It generates a random array of the size given by the user.
+		- It sorts the array in Ascending order using Shaker Sort Algorithm.
+		- It sorts the array in Descending order using Shaker Sort Algorithm.
+		- It prints the time taken to perform both sortings.
+*****************************************************************************************************************************/
+
 #include <bits/stdc++.h>
 
 #include <chrono>
