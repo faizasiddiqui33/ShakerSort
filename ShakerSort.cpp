22ce2985b666d033d1ff5f534cb8f6dcd839eb0b
Faiza Siddiqui <faizasiddiqui@uregina.ca>
2021-11-06 22:55:34 +0300

    adding swap function

--- ShakerSort.cpp
+++ ShakerSort.cpp
@@ -19,6 +19,20 @@
 
 using namespace std;
 
+void swap(int *a, int *b)
+{
+	///temporary variable to help in swapping
+	int temp;
+
+	///storing address of first variable in temporary variable, so that it is not lost
+	temp = *a;
+
+	///replacing the address of first variable with second variable
+	*a = *b;
+
+	///storing the address of first variable which was in temp to second variable
+	*b = temp;
+}
 
 
 int main()
