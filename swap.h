5af1acbc9878ff17abcb65b9318f51eaf04803a1
Faiza Siddiqui <faizasiddiqui@uregina.ca>
2021-11-09 15:47:33 +0300

    Adding comments to header file

--- swap.h
+++ swap.h
@@ -1,3 +1,16 @@
+/*****************************************************************************************************************************
+    Module Name: Swap Function
+    @author: Faiza Fatma Siddiqui
+    Date Created/Modified: 01.11.2021
+    Purpose: To swap values using call by reference
+    Description: This function takes the help of a third temporary variable to interchange the values of two variables
+        @param: int *a - receives the address of a variable to be swapped
+		@param: int *b - receives the address of another variable to be swapped
+        @return null
+	Precondition: address Only integer values should be passed
+	Postcondition: Integer values of 2 numbers are interchanged
+	Method Output: The values of 2 given variables are interchanged
+*****************************************************************************************************************************/
 void swap(int *a, int *b)
 {
 	///temporary variable to help in swapping
